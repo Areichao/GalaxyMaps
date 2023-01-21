db91eccd4edcdcb5bb219c84e348825e29151a9d
Thao Nguyen <thao2.nguyen@ryerson.ca>
2023-01-21 15:42:06 -0500

    added alien

--- galaxy-maps/src/Homepage.js
+++ galaxy-maps/src/Homepage.js
@@ -1,12 +1,14 @@
 import React from "react"
 import "./Homepage.css"
 import saturn from "./images/saturn.png";
+import alien from "./images/alien-spaceship.png";
 
 const Homepage = (props) => {
 
     return (
       <div>
         <img src={saturn} alt='saturn image' />
+        <img src={alien} alt='alien in a spaceship image' />
       </div>
     );
 };
