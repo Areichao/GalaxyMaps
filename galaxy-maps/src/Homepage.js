4182efc716a43645676dfc43534d8973b4e85961
Thao Nguyen <thao2.nguyen@ryerson.ca>
2023-01-21 14:08:42 -0500

    added saturn but it needs fixing

--- galaxy-maps/src/Homepage.js
+++ galaxy-maps/src/Homepage.js
@@ -1,9 +1,15 @@
 import React from "react"
 import "./Homepage.css"
+import saturn from "./images/saturn.png";
 
 
 class Homepage extends React.Component{
-
+    return (
+        <div className="galaxy-space">
+            <img src={saturn} alt="saturn image" />
+        </div>
+   
+    );
 }
 
 export default Homepage
